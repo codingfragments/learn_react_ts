cf7e21db3300d442392df5c181aa1549204783b0
Stefan Marx <smarx@marxworld.net>
2018-05-25 08:43:09 -0700

    Added some state test

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,26 +1,38 @@
-
 import * as React from "react";
-import { lazyInject } from "../services/dependencies.config";
-import { ILogService, LogServiceType } from "../services/LogService";
+import {lazyInject} from "../services/dependencies.config";
+import {ILogService, LogServiceType} from "../services/LogService";
 
 interface ILabelProps {
     label: string;
     prefix: string;
 }
 
-export class CmpLabel extends React.Component<ILabelProps, {}> {
-   @lazyInject(LogServiceType)
-   private LOG: ILogService;
+class LabelState {
+     public name: string = "";
+}
 
-   public render() {
-        return(
-            <h1 onClick={ this.cbClick } >{this.props.prefix} {this.props.label}
-            <img src={ require("../assets/icons/showman.png") }/>
+export class CmpLabel extends React.Component<ILabelProps, LabelState> {
+    public state: Readonly<LabelState>;
+
+    @lazyInject(LogServiceType)
+    private LOG: ILogService;
+
+    public componentWillMount() {
+        this.setState({name: "Stefan"});
+    }
+
+    public render() {
+        return (
+            <h1 onClick={this.cbClick}>{this.props.prefix} {this.props.label} MM {this.state.name}
+                <img src={require("../assets/icons/showman.png")}/>
             </h1>
         );
     }
 
     private cbClick = (e: React.MouseEvent<HTMLElement>) => {
-       this.LOG.log("Logger test", e);
+        this.LOG.log("Logger test", e);
+        const newState = {...this.state, ...{name: "Mike"}};
+        this.setState(newState);
+        // this.state.name = "MIKERL";
     }
 }
