2704a3d720c7339b21c56ce27f843d3d38a94b55
Stefan Marx <smarx@marxworld.net>
2018-05-25 10:59:57 -0700

    Cleanup after Merge

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { AppComponent } from './App';
+import { AppComponent } from "./App";
 
 ReactDOM.render(
     <AppComponent name="Boilerplate" />,
 
-    document.getElementById("application")
+    document.getElementById("application"),
 );
