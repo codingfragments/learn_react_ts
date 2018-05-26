9f86b3dca79caf9e263e0d6f72fc83d02341dbc8
Stefan Marx <smarx@marxworld.net>
2018-05-26 10:52:50 -0700

    Added URL-Loader and cleaned up webpack config file

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,16 @@ import * as ReactDOM from "react-dom";
 import { AppComponent } from "./App";
 
 ReactDOM.render(
-    <AppComponent name="Boilerplate" />,
+    <AppComponent name="Boilerplate-2" />,
     document.getElementById("application"),
 );
 
 /* tslint:disable:object-literal-sort-keys */
 
+const cssImports =  {
+    icons: require("../node_modules/@coreui/icons/css/coreui-icons.css"),
+    flag: require("../node_modules/flag-icon-css/css/flag-icon.css"),
+    fontAwesome: require("../node_modules/font-awesome/css/font-awesome.css"),
+    simpleLine: require("../node_modules/simple-line-icons/css/simple-line-icons.css"),
+    app: require("./scss/style.scss"),
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,151 +1,166 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var WriteFilePlugin =require ('write-file-webpack-plugin');
+var WriteFilePlugin = require('write-file-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var webpack = require('webpack');
+
 MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// Later to add manual minimization
+// UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+// OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 
 
 
-module.exports = (env,argv)=>{
-    
+module.exports = (env, argv) => {
+
     devMode = argv.mode !== 'production';
     splitChunks = true; // Enable SplitChunks here
-    analyze=true;
+    analyze = true;
 
 
     return {
-    //entry: "./src/index.tsx",
-    entry: {
-        app: './src/index.tsx',
-        styles: [
-            /*
-const cssImports =  {
-    icons: require("../node_modules/@coreui/icons/css/coreui-icons.css"),
-    flag: require("../node_modules/flag-icon-css/css/flag-icon.css"),
-    fontAwesome: require("../node_modules/font-awesome/css/font-awesome.css"),
-    simpleLine: require("../node_modules/simple-line-icons/css/simple-line-icons.css"),
-    app: require("./scss/style.scss"),
-};
-*/        './node_modules/@coreui/icons/css/coreui-icons.css',
-          './node_modules/flag-icon-css/css/flag-icon.css',
-          './node_modules/font-awesome/css/font-awesome.css',
-          './node_modules/simple-line-icons/css/simple-line-icons.css',
-          './src/scss/style.scss',
-        ],
-        vendor: [
-          'jquery',
-//          'lodash',
-//          'object-assign',
-//          'q',
-          'react',
-         'react-dom',
-//          'react-router',
-//          'redux',
-//          'redux-thunk',
-//          'toastr'
-        ]
-      },
-    output: {
-        filename: devMode ? '[name].js' : '[name]-[chunkhash].js',
-        chunkFilename:  '[name]-[chunkhash].js',
-        path: __dirname + "/build/app",
-    },
-    optimization: {
-             splitChunks: {
-               chunks: 'all'
-             }
+        //entry: "./src/index.tsx",
+        entry: {
+            app: './src/index.tsx'
         },
 
-    devtool: devMode?"eval-source-map":"source-map"    ,
+        output: {
+            filename: devMode ? '[name].js' : '[name]-[chunkhash].js',
+            chunkFilename: '[name]-[chunkhash].js',
+            path: __dirname + "/build/app",
+        },
+        optimization: {
+            
+            splitChunks: {
+                chunks: 'all',
+                //               chunks: "async",
+                minSize: 30000,
+                minChunks: 1,
+                maxAsyncRequests: 5,
+                maxInitialRequests: 3,
+                automaticNameDelimiter: '~',
+                name: true,
+                cacheGroups: {
+                    vendorModules: {
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: -10
+                    },
+                    default: {
+                        minChunks: 2,
+                        priority: -20,
+                        reuseExistingChunk: true
+                    }
+                }
+            }
+        },
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+        devtool: devMode ? "eval-source-map" : "source-map",
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+        resolve: {
+            // Add '.ts' and '.tsx' as resolvable extensions.
+            extensions: [".ts", ".tsx", ".js", ".json"]
+        },
 
-            // inline svg
-            {
-                test: /\.svg$/,
-                loader: 'svg-url-loader?limit=10000', 
-                options:{
-                    outputPath: 'images/svg'
+        module: {
+            rules: [
+                // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+                {
+                    test: /\.tsx?$/,
+                    loader: "awesome-typescript-loader"
                 },
-              },
-            {test: /\.(gif|png|jpe?g)$/i,
-                use: [
-                    {
+
+                // inline svg
+                {
+                    test: /\.svg$/,
+                    loader: 'svg-url-loader?limit=10000',
+                    options: {
+                        outputPath: 'images/svg'
+                    },
+                },
+                {
+                    test: /\.(gif|png|jpe?g)$/i,
+                    use: [{
                         loader: 'file-loader',
                         options: {
-                          outputPath: 'images'
+                            outputPath: 'images'
                         }
-                  }
-                ],
-            },
-            { test: /\.(woff|woff2|eot|ttf)$/, loader: 'url-loader?limit=10000',
-                    options: {outputPath:"cssAssets"} 
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    { loader:  MiniCssExtractPlugin.loader,},
-                    {
-                    loader: "css-loader", options: {
-                        sourceMap: true
+                    }],
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf)$/,
+                    loader: 'url-loader?limit=10000',
+                    options: {
+                        outputPath: "cssAssets"
                     }
-                }, {
-                    loader: "sass-loader", options: {
-                        sourceMap: true
-                    }
-                }]
-            },
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [{
+                            loader: devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                        },
+                        {
+                            loader: "css-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        }, {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                },
+                // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+                {
+                    enforce: "pre",
+                    test: /\.js$/,
+                    loader: "source-map-loader"
+                },
 
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            hash: devMode?true:false,
-            filename: '../index.html', //relative to root of the application
-            title: 'My Awesome application',
-            myPageHeader: 'Hello World',
-            inject:"header",
-            template: __dirname + "/src/index.html",
-       
-        }),
-        new WriteFilePlugin(),
-        new CopyWebpackPlugin([{
-            from: __dirname+"/public/**/*",
-            to: __dirname+"/build/"
-        }]),
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: devMode ? '[name].css' : '[name].[chunkhash].css',
-            chunkFilename: devMode ? '[name].css' : '[name].[hash].css',
-          }),
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                hash: devMode ? true : false,
+                filename: '../index.html', //relative to root of the application
+                title: 'My Awesome application',
+                myPageHeader: 'Hello World',
+                inject: "header",
+                template: __dirname + "/src/index.html",
 
-          new BundleAnalyzerPlugin({
-              analyzerMode: "disable",
-              generateStatsFile: true,
-              statsFilename: "../webpack-stats.json"
-          })
+            }),
+            new WriteFilePlugin(),
+            new CopyWebpackPlugin([{
+                from: __dirname + "/public/**/*",
+                to: __dirname + "/build/"
+            }]),
+            new MiniCssExtractPlugin({
+                // Options similar to the same options in webpackOptions.output
+                // both options are optional
+                filename: devMode ? '[name].css' : '[name].[chunkhash].css',
+                chunkFilename: devMode ? '[name].css' : '[name].[hash].css',
+            }),
 
-   ],
+            new BundleAnalyzerPlugin({
+                analyzerMode: "disable",
+                generateStatsFile: true,
+                statsFilename: "../webpack-stats.json"
+            }),
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-     //   "react": "React",
-     //   "react-dom": "ReactDOM"
-    },
-}
+
+        ],
+
+        // When importing a module whose path matches one of the following, just
+        // assume a corresponding global variable exists and use that instead.
+        // This is important because it allows us to avoid bundling all of our
+        // dependencies, which allows browsers to cache those libraries between builds.
+        externals: {
+            //   "react": "React",
+            //   "react-dom": "ReactDOM"
+        },
+    }
 }
