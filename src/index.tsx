10416031531d07f7127feede8fabfbe8c985a152
Stefan Marx <smarx@marxworld.net>
2018-05-26 10:02:30 -0700

    Added Webpack Optimization and chuck support

--- src/index.tsx
+++ src/index.tsx
@@ -8,10 +8,4 @@ ReactDOM.render(
 );
 
 /* tslint:disable:object-literal-sort-keys */
-const cssImports =  {
-    icons: require("../node_modules/@coreui/icons/css/coreui-icons.css"),
-    flag: require("../node_modules/flag-icon-css/css/flag-icon.css"),
-    fontAwesome: require("../node_modules/font-awesome/css/font-awesome.css"),
-    simpleLine: require("../node_modules/simple-line-icons/css/simple-line-icons.css"),
-    app: require("./scss/style.scss"),
-};
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,62 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var WriteFilePlugin =require ('write-file-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 MiniCssExtractPlugin = require("mini-css-extract-plugin");
-devMode = process.env.NODE_ENV !== 'production';
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 
 
-module.exports = {
-    entry: "./src/index.tsx",
+
+module.exports = (env,argv)=>{
+    
+    devMode = argv.mode !== 'production';
+    splitChunks = true; // Enable SplitChunks here
+    analyze=true;
+
+
+    return {
+    //entry: "./src/index.tsx",
+    entry: {
+        app: './src/index.tsx',
+        styles: [
+            /*
+const cssImports =  {
+    icons: require("../node_modules/@coreui/icons/css/coreui-icons.css"),
+    flag: require("../node_modules/flag-icon-css/css/flag-icon.css"),
+    fontAwesome: require("../node_modules/font-awesome/css/font-awesome.css"),
+    simpleLine: require("../node_modules/simple-line-icons/css/simple-line-icons.css"),
+    app: require("./scss/style.scss"),
+};
+*/        './node_modules/@coreui/icons/css/coreui-icons.css',
+          './node_modules/flag-icon-css/css/flag-icon.css',
+          './node_modules/font-awesome/css/font-awesome.css',
+          './node_modules/simple-line-icons/css/simple-line-icons.css',
+          './src/scss/style.scss',
+        ],
+        vendor: [
+          'jquery',
+//          'lodash',
+//          'object-assign',
+//          'q',
+          'react',
+         'react-dom',
+//          'react-router',
+//          'redux',
+//          'redux-thunk',
+//          'toastr'
+        ]
+      },
     output: {
-        filename: "bundle.js",
-        path: __dirname + "/build/app"
+        filename: devMode ? '[name].js' : '[name]-[chunkhash].js',
+        chunkFilename:  '[name]-[chunkhash].js',
+        path: __dirname + "/build/app",
     },
+    optimization: {
+             splitChunks: {
+               chunks: 'all'
+             }
+        },
 
-    "devtool": "source-map",
+    devtool: devMode?"eval-source-map":"source-map"    ,
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
@@ -24,11 +68,13 @@ module.exports = {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+            // inline svg
             {
                 test: /\.svg$/,
-                loader: 'svg-url-loader?limit=1024', 
+                loader: 'svg-url-loader?limit=10000', 
                 options:{
-                    outputPath: 'images'
+                    outputPath: 'images/svg'
                 },
               },
             {test: /\.(gif|png|jpe?g)$/i,
@@ -38,23 +84,17 @@ module.exports = {
                         options: {
                           outputPath: 'images'
                         }
-                  },
-                  {
-                    loader: 'image-webpack-loader',
-                    options: {
-                      bypassOnDebug: true,
-                    },
-                  },
+                  }
                 ],
             },
-            { test: /\.(png|woff|woff2|eot|ttf)$/, loader: 'url-loader?limit=100000',
+            { test: /\.(woff|woff2|eot|ttf)$/, loader: 'url-loader?limit=10000',
                     options: {outputPath:"cssAssets"} 
             },
             {
                 test: /\.s?css$/,
-                use: [{
-                    loader:  MiniCssExtractPlugin.loader,
-                }, {
+                use: [
+                    { loader:  MiniCssExtractPlugin.loader,},
+                    {
                     loader: "css-loader", options: {
                         sourceMap: true
                     }
@@ -71,10 +111,11 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            hash: true,
+            hash: devMode?true:false,
             filename: '../index.html', //relative to root of the application
             title: 'My Awesome application',
             myPageHeader: 'Hello World',
+            inject:"header",
             template: __dirname + "/src/index.html",
        
         }),
@@ -86,9 +127,16 @@ module.exports = {
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // both options are optional
-            filename: devMode ? '[name].css' : '[name].[hash].css',
-            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+            filename: devMode ? '[name].css' : '[name].[chunkhash].css',
+            chunkFilename: devMode ? '[name].css' : '[name].[hash].css',
+          }),
+
+          new BundleAnalyzerPlugin({
+              analyzerMode: "disable",
+              generateStatsFile: true,
+              statsFilename: "../webpack-stats.json"
           })
+
    ],
 
     // When importing a module whose path matches one of the following, just
@@ -99,4 +147,5 @@ module.exports = {
      //   "react": "React",
      //   "react-dom": "ReactDOM"
     },
-};
+}
+}
